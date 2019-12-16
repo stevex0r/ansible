eb575873ca5a47712b54c89c5bc09eb4580ba5cc
Steven Mohr <stevex0r@gmail.com>
2019-12-16 16:24:56 -0600

    ansible user script changed

--- scripts/ansible-user.sh
+++ scripts/ansible-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Copy onto new host and execute to create ansible user 
-useradd -m -d /opt/ansible ansible
+useradd -u 1201 -g 1201 -m -d /opt/ansible ansible
 echo -e 'ansible\tALL=(ALL)\tNOPASSWD:\tALL' > /etc/sudoers.d/ansible
 scp -rp root@192.168.1.44:/opt/ansible/.ssh /opt/ansible/
 chown ansible:ansible  -R /opt/ansible
